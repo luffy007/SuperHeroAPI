32fe68e8da82619317f545370aad72c877cd9019
Victor <yuffy07@gmail.com>
2023-02-18 22:06:58 +0100

    Add project files.

---
+++ SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace SuperHeroAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+        public SuperHeroController(DataContext context)
+        {
+            _dataContext = context;
+        }
+        private static List<SuperHero> heroes = new List<SuperHero>
+            {
+                new SuperHero
+                {
+                    Id = 1,
+                    HeroNick = "Super Man",
+                    FirstName = "Clack",
+                    LastName = "Kent",
+                    City = "Smallville"
+                }, 
+                new SuperHero
+                {
+                    Id = 2,
+                    HeroNick = "Batman",
+                    FirstName = "Brush",
+                    LastName = "Wayn",
+                    City = "Gotica"
+                }
+            };
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> Get()
+        {            
+            return Ok(await _dataContext.SuperHeroes.ToListAsync());
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<SuperHero>>> Get(int id)
+        {
+            var hero = await _dataContext.SuperHeroes.FindAsync(id);
+            if(hero == null)
+            {
+                return BadRequest("This hero does not exist.");
+            }
+
+            return Ok(hero);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<SuperHero>>> AddHero(SuperHero hero)
+        {
+            _dataContext.SuperHeroes.Add(hero);
+            await _dataContext.SaveChangesAsync();
+            return Ok(await _dataContext.SuperHeroes.ToListAsync());
+        }
+        [HttpPut]
+        public async Task<ActionResult<List<SuperHero>>> UpdateHero(SuperHero request)
+        {
+            var hero = await _dataContext.SuperHeroes.FindAsync(request.Id);
+            if (hero == null)
+            {
+                return BadRequest("This hero does not exist.");
+            }
+            hero.HeroNick = request.HeroNick;
+            hero.FirstName = request.FirstName;
+            hero.LastName = request.LastName;
+            hero.City = request.City;
+
+            await _dataContext.SaveChangesAsync();
+
+            return Ok(await _dataContext.SuperHeroes.ToListAsync());
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<SuperHero>>> DeleteHero(int id)
+        {
+            var hero = await _dataContext.SuperHeroes.FindAsync(id);
+            if (hero == null)
+            {
+                return BadRequest("This hero does not exist.");
+            }
+            _dataContext.SuperHeroes.Remove(hero);
+            await _dataContext.SaveChangesAsync();
+
+            return Ok(await _dataContext.SuperHeroes.ToListAsync());
+        }
+
+    }
+}
---
+++ SuperHeroAPI/SuperHero.cs
@@ -0,0 +1,11 @@
+﻿namespace SuperHeroAPI
+{
+    public class SuperHero
+    {
+        public int Id { get; set; }
+        public string HeroNick { get; set; } = string.Empty;
+        public string FirstName { get; set;} = string.Empty;
+        public string LastName { get; set; } = string.Empty; 
+        public string City { get; set; } = string.Empty; 
+    }
+}
